d656f6bfd7f283ce10dbac1a7a422a967faffb92
Chloe Wen <cwen1227@gmail.com>
2024-01-02 23:53:28 -0800

    sequentially solution works

--- src/main.cpp
+++ src/main.cpp
@@ -19,11 +19,9 @@ int getCounts(string dataFileName, map<int,int> *counts) {
       char * bufArr = new char[buf.length() + 1];
       strcpy(bufArr, buf.c_str());
       sscanf(bufArr, "%d, %s", &frameIdx, frameVal);
-      // cout << "frameIdx " << frameIdx << "frameVal" << frameVal << endl;
       // update map with counts
       if (strncmp(frameVal, "true", 4) == 0) {
-        cout << "frameIdx " << frameIdx << endl;
-        (*counts)[frameIdx] ++;
+        (*counts)[frameIdx]++;
       }
       
     }
@@ -33,10 +31,6 @@ int getCounts(string dataFileName, map<int,int> *counts) {
     return 1;
   }
 
-  // print counts (TODO: delete later)
-  for (const auto & pair: *counts) {
-    cout << "Key: " << pair.first << ", Value: " << pair.second << endl;
-  }
   return 0;
 }
 
@@ -44,10 +38,38 @@ int main(int argc, char **argv) {
   cout << "test" << argv[1] << endl;
   // create data structure mapping frame indexes to count
   map<int,int> counts; 
-  string dataFileName = string(argv[1]) + "/cam0.txt";
-  if (getCounts(dataFileName, &counts)) {
-    return 1;
+  int numCameras = 6; // TODO: fix
+  for (int i = 0; i < numCameras; i++) {
+    string dataFileName = string(argv[1]) + "/cam" + to_string(i) + ".txt";
+    if (getCounts(dataFileName, &counts)) {
+      return 1;
+    }
   }
+  
+  // print counts (TODO: delete later)
+  for (const auto & pair: counts) {
+    cout << "Key: " << pair.first << ", Value: " << pair.second << endl;
+  }
+
+  // aggregate results
+  int majTrueCount = 0; 
+  int anyTrueCount = 0; 
+  int allTrueCount = 0; 
+  for (const auto & pair: counts) {
+    if (pair.second > numCameras / 2) {
+      majTrueCount++;
+    }
+    if (pair.second > 0) {
+      anyTrueCount++;
+    }
+    if (pair.second == numCameras) {
+      allTrueCount++;
+    }
+  }
+
+  cout << "Frames with majority true: " << majTrueCount << endl;
+  cout << "Frames with any true: " << anyTrueCount << endl;
+  cout << "Frames with all true: " << allTrueCount << endl;
   
   return 0;
 }
