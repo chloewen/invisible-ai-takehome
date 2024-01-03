543ce13b510d410ffd2dc80cf1c8699398fab43f
Chloe Wen <cwen1227@gmail.com>
2024-01-02 23:34:13 -0800

    update count map

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <unordered_map>
+#include <map>
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -7,20 +7,25 @@
 #include <stdio.h>
 using namespace std;
 
-int getCounts(string dataFileName, unordered_map<int,int> *counts) {
+int getCounts(string dataFileName, map<int,int> *counts) {
   // open and read camera file
   ifstream camFile;
   camFile.open(dataFileName);
   string buf;
-  if ( camFile.is_open() ) {
+  if (camFile.is_open()) {
     while (getline(camFile, buf)) {
-      
       int frameIdx;
       char *frameVal;
       char * bufArr = new char[buf.length() + 1];
       strcpy(bufArr, buf.c_str());
       sscanf(bufArr, "%d, %s", &frameIdx, frameVal);
-      cout << buf << frameIdx << frameVal << '\n';
+      // cout << "frameIdx " << frameIdx << "frameVal" << frameVal << endl;
+      // update map with counts
+      if (strncmp(frameVal, "true", 4) == 0) {
+        cout << "frameIdx " << frameIdx << endl;
+        (*counts)[frameIdx] ++;
+      }
+      
     }
     camFile.close();
   } else {
@@ -28,17 +33,17 @@ int getCounts(string dataFileName, unordered_map<int,int> *counts) {
     return 1;
   }
 
-  // // print counts (TODO: delete later)
-  // for (const auto & pair: *counts) {
-  //   cout << "Key: " << pair.first << ", Value: " << pair.second << endl;
-  // }
+  // print counts (TODO: delete later)
+  for (const auto & pair: *counts) {
+    cout << "Key: " << pair.first << ", Value: " << pair.second << endl;
+  }
   return 0;
 }
 
 int main(int argc, char **argv) {
   cout << "test" << argv[1] << endl;
   // create data structure mapping frame indexes to count
-  unordered_map<int,int> counts; 
+  map<int,int> counts; 
   string dataFileName = string(argv[1]) + "/cam0.txt";
   if (getCounts(dataFileName, &counts)) {
     return 1;
