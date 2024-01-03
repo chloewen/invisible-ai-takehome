8ac1ae8e5cc6773906f760bf4e6922fd5092c69b
Chloe Wen <cwen1227@gmail.com>
2024-01-02 22:58:13 -0800

    open and read camera file in getCounts

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
+#include <unordered_map>
+#include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
-int main() {
-  cout << "test" << endl;
+int getCounts(string dataFileName, unordered_map<int,int> *counts) {
+  // open and read camera file
+  ifstream camFile;
+  camFile.open(dataFileName);
+  string frame;
+  if ( camFile.is_open() ) {
+    while ( camFile ) {
+      getline(camFile, frame);
+      cout << frame << '\n';
+    }
+  } else {
+    cout << "Couldn't open file\n";
+    return 1;
+  }
+
+  // // print counts (TODO: delete later)
+  // for (const auto & pair: *counts) {
+  //   cout << "Key: " << pair.first << ", Value: " << pair.second << endl;
+  // }
+  return 0;
+}
+
+int main(int argc, char **argv) {
+  cout << "test" << argv[1] << endl;
+  // create data structure mapping frame indexes to count
+  unordered_map<int,int> counts; 
+  string dataFileName = string(argv[1]) + "/cam0.txt";
+  if (getCounts(dataFileName, &counts)) {
+    return 1;
+  }
+  
   return 0;
 }
